b2b6d12db2f1a8efcb498ca2ff5c304d1394a202
Ahlem FANTAZI <fantaziahlem@gmail.com>
2020-10-20 16:43:30 +0200

    accept method implemented

--- src/main/java/td3/api/visitors/Client.java
+++ src/main/java/td3/api/visitors/Client.java
@@ -1,5 +1,7 @@
 package td3.api.visitors;
 
+import java.util.Objects;
+
 public class Client implements Visitable, PrePostVisitable {
     private  String name;
 
@@ -19,5 +21,18 @@ public class Client implements Visitable, PrePostVisitable {
     @Override
     public void accept(Visitor visitor) {
 
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return name.equals(client.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
--- src/main/java/td3/api/visitors/Ligne.java
+++ src/main/java/td3/api/visitors/Ligne.java
@@ -1,5 +1,7 @@
 package td3.api.visitors;
 
+import java.util.Objects;
+
 public class Ligne implements PrePostVisitable{
     private String name;
     private int  sum;
@@ -15,5 +17,35 @@ public class Ligne implements PrePostVisitable{
     }
     public void accept(Visitor visitor){
 
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+    public void setSum(int sum) {
+        this.sum = sum;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ligne ligne = (Ligne) o;
+        return sum == ligne.sum &&
+                name.equals(ligne.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, sum);
     }
 }
